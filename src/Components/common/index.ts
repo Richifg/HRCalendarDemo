ad4c1d8ebc950549c703572be3dd8de20b1c646b
Ricardo Fernandez <richifg@gmail.com>
2022-10-22 23:13:49 -0600

    Delete src/Components directory

--- src/Components/common/index.ts
+++
@@ -1,5 +0,0 @@
-import Calendar from './Calendar/Calendar';
-import EventInfoDrawer from './EventInfoDrawer/EventInfoDrawer';
-import HalloweenModal from './HalloweenModal/HalloweenModal';
-
-export { Calendar, EventInfoDrawer, HalloweenModal };
--- src/Components/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import CalendarPage from './CalendarPage/CalendarPage';
-import LandingPage from './LandingPage/LandingPage';
-import UnavailablePage from './UnavailablePage/UnavailablePage';
-import PageLayout from './PageLayout/PageLayout';
-
-export { CalendarPage, LandingPage, UnavailablePage, PageLayout };
